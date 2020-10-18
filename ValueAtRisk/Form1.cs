0a846cc06eb3fbbdc967796a5efc877d3323abe3
wxd4cd <wxd4cd@gmail.com>
2020-10-18 23:41:10 +0200

    Save is ready

--- ValueAtRisk/Form1.cs
+++ ValueAtRisk/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ namespace ValueAtRisk
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<decimal> Nyereségek = new List<decimal>();
 
         public Form1()
         {
@@ -24,7 +26,7 @@ namespace ValueAtRisk
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
             
-            List<decimal> Nyereségek = new List<decimal>();
+            
             int intervalum = 30;
             DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
             DateTime záróDátum = new DateTime(2016, 12, 30);
@@ -75,7 +77,22 @@ namespace ValueAtRisk
 
         private void button2_Click(object sender, EventArgs e)
         {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
 
+            saveFileDialog1.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                {
+                    sw.WriteLine("Idoszak,Nyereseg");
+                    for (int i = 0; i < Nyereségek.Count; i++)
+                    {
+                        sw.WriteLine("{0},{1}", i.ToString(), Nyereségek[i].ToString());
+                    }
+                }
+            }
         }
     }
 }
