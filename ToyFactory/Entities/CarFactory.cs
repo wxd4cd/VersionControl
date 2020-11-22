853d01fad2b40c10766eff482792f869aa4f4d5b
wxd4cd <wxd4cd@gmail.com>
2020-11-22 23:14:03 +0100

    Create abstraction with Car Class as well

--- ToyFactory/Entities/CarFactory.cs
+++ ToyFactory/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using ToysFactory.Abstractions;
 
 namespace ToysFactory.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
