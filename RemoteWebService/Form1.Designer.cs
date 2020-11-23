9cce46e65accb10e1e2d82b20e5c978a6b38fdcf
wxd4cd <wxd4cd@gmail.com>
2020-11-23 03:34:00 +0100

    Create Currencies list

--- RemoteWebService/Form1.Designer.cs
+++ RemoteWebService/Form1.Designer.cs
@@ -28,27 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chartRateData = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.CurrencycomboBox = new System.Windows.Forms.ComboBox();
+            this.FromdateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.UntildateTimePicker = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 66);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(380, 300);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // chartRateData
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartRateData.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartRateData.Legends.Add(legend2);
+            this.chartRateData.Location = new System.Drawing.Point(412, 66);
+            this.chartRateData.Name = "chartRateData";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartRateData.Series.Add(series2);
+            this.chartRateData.Size = new System.Drawing.Size(1031, 300);
+            this.chartRateData.TabIndex = 1;
+            this.chartRateData.Text = "chart1";
+            // 
+            // CurrencycomboBox
+            // 
+            this.CurrencycomboBox.FormattingEnabled = true;
+            this.CurrencycomboBox.Items.AddRange(new object[] {
+            "EUR"});
+            this.CurrencycomboBox.Location = new System.Drawing.Point(463, 13);
+            this.CurrencycomboBox.Name = "CurrencycomboBox";
+            this.CurrencycomboBox.Size = new System.Drawing.Size(159, 21);
+            this.CurrencycomboBox.TabIndex = 2;
+            this.CurrencycomboBox.Text = "EUR";
+            this.CurrencycomboBox.SelectedIndexChanged += new System.EventHandler(this.CurrencycomboBox_SelectedIndexChanged);
+            // 
+            // FromdateTimePicker
+            // 
+            this.FromdateTimePicker.Location = new System.Drawing.Point(13, 13);
+            this.FromdateTimePicker.Name = "FromdateTimePicker";
+            this.FromdateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.FromdateTimePicker.TabIndex = 3;
+            this.FromdateTimePicker.ValueChanged += new System.EventHandler(this.FromdateTimePicker_ValueChanged);
+            // 
+            // UntildateTimePicker
+            // 
+            this.UntildateTimePicker.Location = new System.Drawing.Point(243, 13);
+            this.UntildateTimePicker.Name = "UntildateTimePicker";
+            this.UntildateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.UntildateTimePicker.TabIndex = 4;
+            this.UntildateTimePicker.ValueChanged += new System.EventHandler(this.UntildateTimePicker_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1455, 402);
+            this.Controls.Add(this.UntildateTimePicker);
+            this.Controls.Add(this.FromdateTimePicker);
+            this.Controls.Add(this.CurrencycomboBox);
+            this.Controls.Add(this.chartRateData);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +113,10 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRateData;
+        private System.Windows.Forms.ComboBox CurrencycomboBox;
+        private System.Windows.Forms.DateTimePicker FromdateTimePicker;
+        private System.Windows.Forms.DateTimePicker UntildateTimePicker;
     }
 }
 
--- RemoteWebService/Form1.cs
+++ RemoteWebService/Form1.cs
@@ -5,31 +5,49 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace RemoteWebService
 {
     public partial class Form1 : Form
     {
-        List<RateData> Rates = new List<RateData>();
+        BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<string> Currencies = new BindingList<string>();
         public Form1()
         {
+            
             InitializeComponent();
-            FetchExchangeRates();
+            ParsingCurrencies(FetchCurrencies());
             dataGridView1.DataSource = Rates;
+            CurrencycomboBox.DataSource = Currencies;
+            // FetchExchangeRates();
+            RefreshData();
+            
         }
-        public void FetchExchangeRates()
+
+        public void RefreshData()
+        {
+            Rates.Clear();
+            ParsingXML(FetchExchangeRates());
+            
+            FillChart();
+
+        }
+        public string FetchExchangeRates()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = CurrencycomboBox.SelectedItem.ToString(),
+                startDate = FromdateTimePicker.Value.ToString(),
+                endDate = UntildateTimePicker.Value.ToString()
             };
 
             // Ebben az esetben a "var" a GetExchangeRates visszatérési értékéből kapja a típusát.
@@ -39,7 +57,106 @@ namespace RemoteWebService
             // Ebben az esetben a "var" a GetExchangeRatesResult property alapján kapja a típusát.
             // Ezért a result változó valójában string típusú.
             var result = response.GetExchangeRatesResult;
+            return result;
         }
 
+        public string FetchCurrencies()
+        {
+            var mnbServiece = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetCurrenciesRequestBody();
+            var respone = mnbServiece.GetCurrencies(request);
+            var result = respone.GetCurrenciesResult;
+            return result;
+        }
+
+        public void ParsingCurrencies(string result)
+        {
+            MessageBox.Show(result.ToString());
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            // Végigmegünk a dokumentum fő elemének gyermekein
+            foreach (XmlElement element in xml.DocumentElement.ChildNodes[0])
+            {
+                Currencies.Add(element.InnerText.ToString());
+            }
+        }
+
+            public void ParsingXML(string result)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            // Végigmegünk a dokumentum fő elemének gyermekein
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                // Létrehozzuk az adatsort és rögtön hozzáadjuk a listához
+                // Mivel ez egy referencia típusú változó, megtehetjük, hogy előbb adjuk a listához és csak később töltjük fel a tulajdonságait
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                if (childElement == null)
+                    continue;
+
+                rate.Currency = childElement.GetAttribute("curr");
+
+                // Érték
+                // Decimal separator is a hard thing, especial when input comes with non-English decimal separator, but the Studio default language is English...
+                //string message = string.Format("Parsing Unit and Value: {0} -- {1}, separator: {3}", childElement.GetAttribute("unit").ToString(), childElement.InnerText.ToString(), separator);
+                
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                decimal value;
+                if (System.Threading.Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName == "hu")
+                {
+                    value = decimal.Parse(childElement.InnerText);
+                } else
+                {
+                    value = decimal.Parse(childElement.InnerText.Replace(',','.'));
+                }
+                
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+        }
+
+        public void FillChart()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void FromdateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void UntildateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void CurrencycomboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
     }
 }
