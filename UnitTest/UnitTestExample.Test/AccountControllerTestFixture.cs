48becea2aaa651d27163d390dd1eb6d6d8578ce5
wxd4cd <wxd4cd@gmail.com>
2020-11-30 00:38:10 +0100

    HappyPath implemented

--- UnitTest/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTest/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -50,5 +50,24 @@ namespace UnitTestExample.Test
             Assert.AreEqual(expectedResult, actualResult);
 
         }
+
+        [
+            Test,
+            TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+        ]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.Register(email, password);
+
+            // Assert
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
     }
 }
